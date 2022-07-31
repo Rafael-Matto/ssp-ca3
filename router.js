b51f73388b8367d16657bac5c1d8795bf380e99b
Rafael Somattos <rafael.somattos@gmail.com>
2022-07-31 15:50:06 +0500

    Job retrival , Job Detail , Post new Job functions

--- router.js
+++ router.js
@@ -3,7 +3,8 @@ const fs = require("fs");
 const express = require("express")
 //requiring express router for routes
 const router = express.Router();
-
+//requiring sanitize-html package to safe play with user's data on server side
+const sanitizeHTML = require('sanitize-html')
 //getting jobs data from JSON file
 const loadJobs = () => {
   try {
@@ -16,6 +17,27 @@ const loadJobs = () => {
 }
 //storing job data to jobs for further use in project
 const jobs = loadJobs()
+//home route
+router.get("/", function (req, res) {
+  res.render("home", { jobsdata: jobs,success:req.flash('success'),errors:req.flash('errors') })
+})
+//route for posting new job where we data entry our new job post
+router.get("/postJob", function (req, res) {
+  res.render("postJob");
+});
+//route for getting specific job detail
+router.get("/jobdetail/:id",(req,res)=>{
+    let jobDetailObject
+    jobs.forEach(job => {
+    if(job.id == req.params.id){
+        jobDetailObject = job
+    }
+  })
+  
+  res.render('jobdetails',{job:jobDetailObject})
+
+})
+
 
 
 //writing job data in JSON file
